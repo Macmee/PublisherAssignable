0ed7b7ba6065af971af04222a50f82f9ff7e4323
David Zorychta <macmee@gmail.com>
2022-01-14 20:38:04 -0800

    Initial Commit

---
+++ Shared/PublishedViewModelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PublishedViewModelsApp.swift
+//  Shared
+//
+//  Created by David Zorychta on 1/14/22.
+//
+
+import SwiftUI
+
+@main
+struct PublishedViewModelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
